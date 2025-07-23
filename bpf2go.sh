f9ddbced539da6d2a18a39dec96d855251459ff1
flowerinthenight <root@flowerinthenight.com>
2025-07-24 03:42:50 +0900

    ci skip: move generate to sh

--- bpf2go.sh
+++ bpf2go.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 bpf2go \
-    -tags linux \
-    -cflags "-O2 -g -Wall -Werror" \
-    -type event \
-    -output-dir bpf/ \
-    -go-package bpf \
-    Bpf ./bpf/bpf.c -- \
-    -I./libbpf/src \
-    -I./vmlinux.h/include/x86_64
+  -tags linux \
+  -cflags "-O2 -g -Wall -Werror" \
+  -type event \
+  -output-dir bpf/ \
+  -go-package bpf \
+  Bpf ./bpf/bpf.c \
+  -- \
+  -I./libbpf/src \
+  -I./vmlinux.h/include/x86_64
