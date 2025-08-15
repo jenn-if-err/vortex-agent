eb2e4ccdd57de653e6b16a716620da2f62d7343d
flowerinthenight <root@flowerinthenight.com>
2025-08-15 22:00:09 +0900

    ci skip: wip test sockops attachment

--- bpf/sk.c
+++ bpf/sk.c
@@ -5,46 +5,78 @@
 #ifndef __BPF_VORTEX_SK_C
 #define __BPF_VORTEX_SK_C
 
-struct sock_key {
-    __u32 sip4;
-    __u32 dip4;
-    __u32 sport;
-    __u32 dport;
-};
-
+/*
 struct {
     __uint(type, BPF_MAP_TYPE_SOCKHASH);
     __uint(max_entries, 1024);
-    __type(key, struct sock_key);
-    __type(value, __u32);
+    __type(key, __u32);
+    __type(value, __u64);
 } sock_map SEC(".maps");
+*/
 
 SEC("sockops")
-int bpf_sockops_handler(struct bpf_sock_ops *sk_ops) {
-    if (sk_ops->family != AF_INET || sk_ops->op != BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB)
-        return BPF_OK;
+int bpf_sockops_handler(struct bpf_sock_ops *ctx) {
+    /*
+    __u32 lip4;
+    __u32 rip4;
+    __u32 lport;
+    __u32 rport;
+    */
 
-    struct sock_key key = {
-        .sip4 = sk_ops->local_ip4,
-        .dip4 = sk_ops->remote_ip4,
-        .sport = sk_ops->local_port,
-        .dport = bpf_ntohl(sk_ops->remote_port),
-    };
+    __u32 op = ctx->op;
 
-    bpf_printk("bpf_sockops_handler: established connection %pI4:%u -> %pI4:%u", &key.sip4, key.sport, &key.dip4,
-               key.dport);
+    switch (op) {
+    case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB: /* we are client */
+        break;
 
-    int ret = bpf_sock_hash_update(sk_ops, &sock_map, &key, BPF_NOEXIST);
-    if (ret != 0)
-        bpf_printk("bpf_sockops_handler: failed to update sock_map: %d", ret);
+        /*
+        lip4 = ctx->local_ip4;
+        rip4 = ctx->remote_ip4;
+        lport = ctx->local_port;
+        rport = bpf_ntohl(ctx->remote_port);
+        bpf_printk("sockops: ACTIVE conn established: %pI4:%u -> %pI4:%u", &lip4, lport, &rip4, rport);
+        */
+
+    case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB: /* we are server */
+        break;
+
+        /*
+        lip4 = ctx->local_ip4;
+        rip4 = ctx->remote_ip4;
+        lport = ctx->local_port;
+        rport = bpf_ntohl(ctx->remote_port);
+
+        bpf_printk("sockops: PASSIVE conn established: %pI4:%u -> %pI4:%u", &lip4, lport, &rip4, rport);
+        */
+
+        /*
+        struct sock_key key = {
+            .sip4 = sk_ops->local_ip4,
+            .dip4 = sk_ops->remote_ip4,
+            .sport = sk_ops->local_port,
+            .dport = bpf_ntohl(sk_ops->remote_port),
+        };
+
+        bpf_printk("sockops: established connection %pI4:%u -> %pI4:%u", &key.sip4, key.sport, &key.dip4, key.dport);
+
+        int ret = bpf_sock_hash_update(ctx, &sock_map, &rport, BPF_ANY);
+        if (ret != 0) {
+            bpf_printk("sockops: failed to update sock_hash: %d", ret);
+        }
+        */
+    }
+
+    /* BPF processing for messages on this socket. We do this by setting */
+    /* bpf_sock_ops_cb_flags_set(sk_ops, BPF_SOCK_OPS_PARSE_ALL_HDR_OPT_CB_FLAG); */
 
     return BPF_OK;
 }
 
+/*
 SEC("sk_msg")
 int bpf_sk_msg_handler(struct sk_msg_md *msg) {
-    bpf_printk("bpf_sk_msg_handler: message of size %d intercepted on a monitored socket", msg->size);
     return SK_PASS;
 }
+*/
 
 #endif /* __BPF_VORTEX_SK_C */
--- subcmds/run.go
+++ subcmds/run.go
@@ -32,6 +32,7 @@ import (
 	"github.com/flowerinthenight/vortex-agent/params"
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
+	"golang.org/x/sys/unix"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -210,53 +211,68 @@ func run(ctx context.Context, done chan error) {
 	}
 
 	// NOTE: TEST ONLY: TO BE REMOVED LATER (start).
-	cgroupPath := "/sys/fs/cgroup/"
-	sockMapPath := "/sys/fs/bpf/sk_msg_sock_map"
-
-	// Pinning the map is necessary for the sk_msg program to find it.
-	// We must remove any previous pin first.
-	os.Remove(sockMapPath)
-	if err := os.MkdirAll(filepath.Dir(sockMapPath), 0755); err != nil {
-		glog.Errorf("MkdirAll (%v) failed: %v", sockMapPath, err)
-		return
-	}
-
-	// Pin the sock_hash map to the BPF filesystem. This is how the sock_ops and sk_msg
-	// programs will share the map.
-	if err := objs.SockMap.Pin(sockMapPath); err != nil {
-		glog.Errorf("pinning %v failed: %v", sockMapPath, err)
-	} else {
-		defer objs.SockMap.Unpin()
-		glog.Infof("%v pinned", sockMapPath)
-	}
-
-	// The sock_ops program is attached to the cgroup.
-	l, err = link.AttachCgroup(link.CgroupOptions{
-		Path:    cgroupPath,
-		Attach:  ebpf.AttachCGroupSockOps,
-		Program: objs.BpfSockopsHandler,
-	})
-
+	cgroupPath, err := findCgroupPath()
 	if err != nil {
-		glog.Errorf("attaching sock_ops to %v failed: %v", cgroupPath, err)
+		glog.Errorf("findCgroupPath failed: %v", err)
 	} else {
-		hostLinks = append(hostLinks, l)
-		glog.Infof("sock_ops attached to %s", cgroupPath)
-	}
+		// sockMapPath := "/sys/fs/bpf/sk_msg_sock_map"
 
-	// The sk_msg program must be attached to the sock_hash map using a raw link.
-	// This is the general-purpose attachment function for link types that don't
-	// have a dedicated helper.
-	err = link.RawAttachProgram(link.RawAttachProgramOptions{
-		Program: objs.BpfSkMsgHandler,
-		Target:  objs.SockMap.FD(),
-		Attach:  ebpf.AttachSkMsgVerdict,
-	})
+		// Pinning the map is necessary for the sk_msg program to find it.
+		// We must remove any previous pin first.
+		// os.Remove(sockMapPath)
+		// if err := os.MkdirAll(filepath.Dir(sockMapPath), 0755); err != nil {
+		// 	glog.Errorf("MkdirAll (%v) failed: %v", sockMapPath, err)
+		// } else {
+		// 	// Pin the sock_hash map to the BPF filesystem. This is how the
+		// 	// sock_ops and sk_msg programs will share the map.
+		// 	if err := objs.SockMap.Pin(sockMapPath); err != nil {
+		// 		glog.Errorf("pinning %v failed: %v", sockMapPath, err)
+		// 	} else {
+		// 		glog.Infof("%v pinned", sockMapPath)
+		// 		defer func() {
+		// 			objs.SockMap.Unpin()
+		// 			os.Remove(sockMapPath)
+		// 		}()
+		// 	}
+		// }
 
-	if err != nil {
-		glog.Errorf("RawAttachProgram failed: %v", err)
-	} else {
-		glog.Infof("sk_msg program attached to %v", sockMapPath)
+		// The sock_ops program is attached to the cgroup.
+		// l, err = link.AttachCgroup(link.CgroupOptions{
+		// 	Path:    cgroupPath,
+		// 	Attach:  ebpf.AttachCGroupSockOps,
+		// 	Program: objs.BpfSockopsHandler,
+		// })
+
+		// if err != nil {
+		// 	glog.Errorf("attaching sock_ops to %v failed: %v", cgroupPath, err)
+		// } else {
+		// 	hostLinks = append(hostLinks, l)
+		// 	glog.Infof("sock_ops attached to %s", cgroupPath)
+		// }
+
+		// The sk_msg program must be attached to the sock_hash map using a raw link.
+		// This is the general-purpose attachment function for link types that don't
+		// have a dedicated helper.
+		// err = link.RawAttachProgram(link.RawAttachProgramOptions{
+		// 	Program: objs.BpfSkMsgHandler,
+		// 	Target:  objs.SockMap.FD(),
+		// 	Attach:  ebpf.AttachSkMsgVerdict,
+		// })
+
+		// if err != nil {
+		// 	glog.Errorf("RawAttachProgram failed: %v", err)
+		// } else {
+		// 	glog.Infof("sk_msg program attached to sock_map")
+		// 	defer func() {
+		// 		link.RawDetachProgram(link.RawDetachProgramOptions{
+		// 			Program: objs.BpfSkMsgHandler,
+		// 			Target:  objs.SockMap.FD(),
+		// 			Attach:  ebpf.AttachSkMsgVerdict,
+		// 		})
+		// 	}()
+		// }
+
+		_ = cgroupPath
 	}
 	// NOTE: TEST ONLY: TO BE REMOVED LATER (end).
 
@@ -1128,4 +1144,21 @@ func setupUprobes(ex *link.Executable, links *[]link.Link, objs *bpf.BpfObjects)
 	} else {
 		*links = append(*links, l)
 	}
+}
+
+func findCgroupPath() (string, error) {
+	cgroupPath := "/sys/fs/cgroup"
+
+	var st syscall.Statfs_t
+	err := syscall.Statfs(cgroupPath, &st)
+	if err != nil {
+		return "", err
+	}
+
+	isCgroupV2Enabled := st.Type == unix.CGROUP2_SUPER_MAGIC
+	if !isCgroupV2Enabled {
+		cgroupPath = filepath.Join(cgroupPath, "unified")
+	}
+
+	return cgroupPath, nil
 }
