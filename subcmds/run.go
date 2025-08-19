cdcdbfc7ef51f12da75b7dd512d3b29ba7892476
Jennifer Tongco <jennifer.j.tongco@gmail.com>
2025-08-19 16:09:00 +0800

    fix: update condition for saving chunk to Spanner based on event command

--- subcmds/run.go
+++ subcmds/run.go
@@ -1004,7 +1004,7 @@ func run(ctx context.Context, done chan error) {
 					}()
 					internalglog.LogInfo(line.String())
 
-					if params.RunfSaveDb {
+					if strings.Contains(fmt.Sprintf("%s", event.Comm), "node") || (strings.Contains(fmt.Sprintf("%s", event.Buf[:]), "python")) && params.RunfSaveDb {
 						fmt.Printf("[DEBUG] Saving chunk to Spanner for id=%v/%v, idx=%v\n", event.Tgid, event.Pid, event.ChunkIdx)
 						cols := []string{
 							"id",
