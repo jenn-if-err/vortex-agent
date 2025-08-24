3a3e18bf75b2211053f939b7d9af231d182ff70e
flowerinthenight <root@flowerinthenight.com>
2025-08-24 23:43:24 +0900

    ci skip: update local build script

--- localbuild.sh
+++ localbuild.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Local build only; environment specific.
-docker build --rm -t vortex-agent .
-docker tag vortex-agent asia-docker.pkg.dev/mobingi-main/asia-pub/vortex-agent:$1
-docker push asia-docker.pkg.dev/mobingi-main/asia-pub/vortex-agent:$1
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc) -f
+DOCKER_BUILDKIT=0 docker build --rm -t vortex-agent .
+DOCKER_BUILDKIT=0 docker tag vortex-agent asia-docker.pkg.dev/mobingi-main/asia-pub/vortex-agent:$1
+DOCKER_BUILDKIT=0 docker push asia-docker.pkg.dev/mobingi-main/asia-pub/vortex-agent:$1
+DOCKER_BUILDKIT=0 docker rmi $(docker images --filter "dangling=true" -q --no-trunc) -f
 sed -i -e 's/image\:\ asia-docker.pkg.dev\/mobingi\-main\/asia\-pub\/vortex\-agent[\:@].*$/image\:\ asia-docker.pkg.dev\/mobingi\-main\/asia\-pub\/vortex\-agent\:'$1'/g' daemonset.yaml
