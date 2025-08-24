902f9d3e863e08b2809750f66e801ea24a7dbdfe
flowerinthenight <root@flowerinthenight.com>
2025-08-24 14:59:11 +0900

    ci skip: rm tests, mv script

---
+++ tools/standup-gke.sh
@@ -0,0 +1,15 @@
+gcloud container clusters create bpf-ch \
+       --workload-pool=alphaus-dashboard.svc.id.goog \
+       --project=alphaus-dashboard \
+       --zone=asia-northeast1-b \
+       --machine-type=e2-standard-2 \
+       --scopes=default \
+       --enable-autoscaling \
+       --enable-vertical-pod-autoscaling \
+       --min-nodes=1 --max-nodes=4 \
+       --maintenance-window=16:00 \
+       --network=default \
+       --subnetwork=default \
+       --enable-ip-alias \
+       --addons=HttpLoadBalancing \
+       --release-channel=regular
